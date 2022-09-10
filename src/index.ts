499ea18dedbf9888601eb3bae77c8757874b21d6
Kyle B. Lackinger <67078189+kbl40@users.noreply.github.com>
2022-09-10 19:29:58 -0400

    token account instructions added

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,16 @@
 import { initializeKeypair } from "./initializeKeypair"
 import createMint from "./createMint"
 import * as web3 from "@solana/web3.js"
+import {
+  PublicKey
+} from '@solana/web3.js'
+import { 
+  ACCOUNT_SIZE,
+  createInitializeAccountInstruction,
+  createInitializeMintInstruction, 
+  getMinimumBalanceForRentExemptMint, 
+  MINT_SIZE, 
+  TOKEN_PROGRAM_ID } from '@solana/spl-token'
 import * as token from '@solana/spl-token'
 import {
   Metaplex,
@@ -16,28 +26,72 @@ import {
 } from '@metaplex-foundation/mpl-token-metadata'
 import * as fs from 'fs'
 
-// Mint account helper function
-async function createNewMint(
-  connection: web3.Connection,
-  payer: web3.Keypair,
-  mintAuthority: web3.PublicKey,
-  freezeAuthority: web3.PublicKey | null,
-  decimals: number
-) {
-  const tokenMint = await token.createMint(
-    connection,
-    payer,
-    mintAuthority,
-    freezeAuthority,
-    decimals
-  )
-}
-
 async function main() {
   const connection = new web3.Connection(web3.clusterApiUrl("devnet"))
   const user = await initializeKeypair(connection)
+  const mintKeypair = web3.Keypair.generate()
+  const accountKeypair = web3.Keypair.generate()
+
+  const programId = TOKEN_PROGRAM_ID
+  const accountProgramId = TOKEN_PROGRAM_ID
+  const decimals = 2
 
   console.log("PublicKey:", user.publicKey.toBase58())
+
+  const lamportsMint = await token.getMinimumBalanceForRentExemptMint(connection)
+  const lamportsAccount = await token.getMinimumBalanceForRentExemptAccount(connection)
+
+  // const associatedTokenAccount = await token.createAssociatedTokenAccount(
+  //   connection,
+  //   user,
+  //   keypair.publicKey,
+  //   user.publicKey
+  // )
+
+  const transaction = new web3.Transaction().add(
+    // Create Mint Account
+    web3.SystemProgram.createAccount({
+        fromPubkey: user.publicKey,
+        newAccountPubkey: mintKeypair.publicKey,
+        space: MINT_SIZE,
+        lamports: lamportsMint,
+        programId
+    }),
+    // Initialize it as a Mint Account
+    createInitializeMintInstruction(
+        mintKeypair.publicKey,
+        decimals,
+        user.publicKey,
+        user.publicKey,
+        programId
+    ),
+    // Create Token Account
+    web3.SystemProgram.createAccount({
+      fromPubkey: user.publicKey,
+      newAccountPubkey: accountKeypair.publicKey,
+      space: ACCOUNT_SIZE,
+      lamports: lamportsAccount,
+      programId: accountProgramId
+    }),
+    // Initialize Token Account
+    createInitializeAccountInstruction(
+      accountKeypair.publicKey,
+      mintKeypair.publicKey,
+      user.publicKey,
+      accountProgramId
+    )
+    
+  )
+
+  const txSig = await web3.sendAndConfirmTransaction(
+      connection,
+      transaction,
+      [user, mintKeypair, accountKeypair]
+  )
+
+  console.log(
+    `Token Mint & Accont Creation: https://explorer.solana.com/tx/${txSig}?cluster=devnet`
+  )
 
   /*
   Sample transaction with multiple instructions
