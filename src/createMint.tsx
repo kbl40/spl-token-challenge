8927d095365287f9c9d61a6f984722d12fd016be
Kyle B. Lackinger <67078189+kbl40@users.noreply.github.com>
2022-09-10 17:55:09 -0400

    added createMint function

---
+++ src/createMint.tsx
@@ -0,0 +1,44 @@
+import * as web3 from '@solana/web3.js'
+import * as token from '@solana/spl-token'
+import { createInitializeMintInstruction, getMinimumBalanceForRentExemptMint, MINT_SIZE, TOKEN_PROGRAM_ID } from '@solana/spl-token'
+
+export async function createMint(
+    connection: web3.Connection,
+    payer: web3.Keypair,
+    mintAuthority: web3.PublicKey,
+    freezeAuthority: web3.PublicKey | null,
+    decimals: number,
+    keypair = web3.Keypair.generate(),
+    confirmOptions?: web3.ConfirmOptions ,
+    programId = TOKEN_PROGRAM_ID
+): Promise<web3.PublicKey> {
+    const lamports = await getMinimumBalanceForRentExemptMint(connection)
+
+    const transaction = new web3.Transaction().add(
+        web3.SystemProgram.createAccount({
+            fromPubkey: payer.publicKey,
+            newAccountPubkey: keypair.publicKey,
+            space: MINT_SIZE,
+            lamports,
+            programId,
+        }),
+        createInitializeMintInstruction(
+            keypair.publicKey,
+            decimals,
+            mintAuthority,
+            freezeAuthority,
+            programId
+        )
+    )
+
+    await web3.sendAndConfirmTransaction(
+        connection,
+        transaction,
+        [payer, keypair],
+        confirmOptions
+    )
+
+    return keypair.publicKey 
+}
+
+export default function createMint
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,74 @@
 import { initializeKeypair } from "./initializeKeypair"
 import * as web3 from "@solana/web3.js"
+import * as token from '@solana/spl-token'
+import {
+  Metaplex,
+  keypairIdentity,
+  bundlrStorage,
+  toMetaplexFile,
+  findMetadataPda
+} from '@metaplex-foundation/js'
+import {
+  DataV2, 
+  createCreateMetadataAccountV2Instruction,
+  createUpdateMetadataAccountV2Instruction
+} from '@metaplex-foundation/mpl-token-metadata'
+import * as fs from 'fs'
+
+// Mint account helper function
+async function createNewMint(
+  connection: web3.Connection,
+  payer: web3.Keypair,
+  mintAuthority: web3.PublicKey,
+  freezeAuthority: web3.PublicKey,
+  decimals: number
+) {
+  const tokenMint = await token.createMint(
+    connection,
+    payer,
+    mintAuthority,
+    freezeAuthority,
+    decimals
+  )
+}
 
 async function main() {
   const connection = new web3.Connection(web3.clusterApiUrl("devnet"))
   const user = await initializeKeypair(connection)
 
   console.log("PublicKey:", user.publicKey.toBase58())
+
+  /*
+  Sample transaction with multiple instructions
+  
+  This creates a new Account and initializes it as a Mint Account
+  const transaction = new Transaction.add(
+    SystemProgram.createAccount({
+      fromPubkey: payer.publicKey,
+      newAccountPubkey: keypair.publicKey,
+      space: MINT_SIZE,
+      lamports,
+      programId,
+    }),
+    createInitializeMintInstruction(
+      keypair.publicKey,
+      decimals,
+      mintAuthority,
+      freezeAuthority,
+      programId
+    )
+  )
+
+  // Send the transaction
+  await sendAndConfirmTransaction(
+    connection,
+    transaction,
+    [payer, keypair],
+    confirmOptions
+  )
+
+  return keypair.publicKey
+  */
 }
 
 main()
